66eafd59dfe42f64724941bb6a183e9bc0fbefc9
boblinthorst <bob.linthorst@gmail.com>
2019-10-03 15:16:47 +0200

    send message as the user to The chosen slack channel

--- 2019-10-03/nodejs-slack-integration/slack-integration.js
+++ 2019-10-03/nodejs-slack-integration/slack-integration.js
@@ -5,7 +5,15 @@ const request = require('request');
 // This is Bob's access token.
 const token = "";
 const web = new WebClient( token );
-const channelID = "";
+const conversationId = "CP2ASJ4H4";
+
+(async () => {
+	// See: https://api.slack.com/methods/chat.postMessage
+	const res = await web.chat.postMessage({ channel: conversationId, text: 'Hello there' });
+
+	// `res` contains information about the posted message
+	console.log('Message sent: ', res.ts);
+})();
 
 // Instantiates Express and assigns our app variable to it
 var slackIntegration = express();
