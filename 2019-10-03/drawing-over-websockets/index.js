8b0724fdeadf65f3c04e01ce63aab8acf8b30fcf
Jimmy Comack <jimmy@yoast.com>
2019-10-03 14:26:43 +0200

    Added a canvas

--- 2019-10-03/drawing-over-websockets/index.js
+++ 2019-10-03/drawing-over-websockets/index.js
@@ -1,22 +1,22 @@
 const express = require( 'express' );
+const path = require( 'path' );
 
 const app = express();
 
-app.use( express.static( 'public' ) );
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/', ( request, response ) => {
-	response.send( 'Hello' );
+	response.sendFile(path.join(__dirname + '/index.html'));
 } );
 
 // Handle 404
-app.use(function(req, res) {
-	res.send('404: Not Found', 404);
-
+app.use(function(request, response) {
+	response.sendFile(path.join(__dirname + '/404.html'));
 });
 
 // Handle 500
-app.use(function(error, req, res, next) {
-	res.send('500: Internal Server Error', 500);
+app.use(function(error, request, response, next) {
+	response.send('500: Internal Server Error', 500);
 });
 
 app.listen( 8081 );
