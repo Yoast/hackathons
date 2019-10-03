5e451b5df427de8c913b3d55a68a9c365c836b37
boblinthorst <bob.linthorst@gmail.com>
2019-10-03 14:21:36 +0200

    Basic ngrok setup to receive slack requests locally

--- 2019-10-03/nodejs-slack-integration/server.js
+++ 2019-10-03/nodejs-slack-integration/server.js
@@ -1,16 +1,58 @@
-'use strict';
+// Import express and request modules
+var express = require('express');
+var request = require('request');
 
-const express = require('express');
+// Store our app's ID and Secret. These we got from Step 1.
+// For this tutorial, we'll keep your API credentials right here. But for an actual app, you'll want to  store them securely in environment variables.
+var clientId = '784015611431.782385626560';
+var clientSecret = '5d77e0b5500c3c21c506f7ad71760dd8';
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+// Instantiates Express and assigns our app variable to it
+var app = express();
 
-// App
-const server = express();
-server.get('/', ( req, res) => {
-  res.send( 'Hello world nerds for de\n' );
+// Again, we define a port we want to listen to
+const PORT=4390;
+
+// Lets start our server
+app.listen(PORT, function () {
+	//Callback triggered when server is successfully listening. Hurray!
+	console.log( "Example app listening on port " + PORT );
 });
 
-server.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+
+// This route handles GET requests to our root ngrok address and responds with the same "Ngrok is working message" we used before
+app.get('/', function(req, res) {
+	res.send('Ngrok is working! Path Hit: ' + req.url);
+});
+
+// This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
+app.get('/oauth', function(req, res) {
+	// When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
+	if (!req.query.code) {
+		res.status(500);
+		res.send({"Error": "Looks like we're not getting code."});
+		console.log("Looks like we're not getting code.");
+	} else {
+		// If it's there...
+
+		// We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
+		request({
+			url: 'https://slack.com/api/oauth.access', //URL to hit
+			qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
+			method: 'GET', //Specify the method
+
+		}, function (error, response, body) {
+			if (error) {
+				console.log(error);
+			} else {
+				res.json(body);
+
+			}
+		})
+	}
+});
+
+// Route the endpoint that our slash cool will point to and send back a simple response to indicate that ngrok is working
+app.post('/exists', function(req, res) {
+	res.send('Your ngrokuuus tunnel is up and running!');
+});
--- 2019-10-03/nodejs-slack-integration/slack-integration.js
+++ 2019-10-03/nodejs-slack-integration/slack-integration.js
@@ -1,3 +1,30 @@
 const { WebClient } = require('@slack/web-api');
+const express = require('express');
+const request = require('request');
 
-console.log('Getting started with Node Slack SDK');
+// This is Bob's access token.
+const token = "xoxp-784015611431-784015612471-784611786150-d89c9eaf1cb417a05d3583d85910897f";
+
+// Instantiates Express and assigns our app variable to it
+var slackIntegration = express();
+
+// Hello world.
+slackIntegration.get('/', ( req, res) => {
+	res.send( 'Hello world nerds for de\n' );
+});
+
+// Again, we define a port we want to listen to
+const PORT=8080;
+
+// Lets start our server
+slackIntegration.listen( PORT, function () {
+	//Callback triggered when server is successfully listening. Hurray!
+	console.log( "The slack integration app listening on port " + PORT );
+});
+
+// Route the endpoint that our slash cool will point to and send back a simple response to indicate that ngrok is working
+slackIntegration.post('/exists', function( req, res ) {
+	res.send( 'This is the result of your slash command.' );
+} );
+
+console.log( 'Getting started with Node Slack SDK' );
