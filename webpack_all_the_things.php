9c9f3da4fd461df863fef98163c7b48588ac3e27
Dieter <dieter@yoast.com>
2020-06-04 11:36:23 +0200

    Add plugin menu and load basic JS file

--- webpack_all_the_things.php
+++ webpack_all_the_things.php
@@ -6,6 +6,22 @@
    Version: 1.0
    Author: Agnieszka & Dieter   
    Author URI: http://webpacktest.com
-   License: GPL2
+   License:   GPL2
    */
+
+   // Add the menu to the admin 
+  function add_webpack_menu_to_admin() {
+      add_menu_page( "Webpack All the things", "Webpack All the things", 1, "Webpack All the things", "render_webpack_menu" );
+   }
+   add_action('admin_menu', 'add_webpack_menu_to_admin');
+
+   // Render the Webpack page. 
+   function render_webpack_menu() {
+      $dir = plugin_dir_url( __FILE__ );
+      echo "Hello. We are going to load some Javascript here.";
+      echo "<br>";
+      echo "We are loading the following JS file: ". $dir . 'js/init.js';
+      echo "<br>";
+      wp_enqueue_script( 'js-init-file', $dir . 'js/init.js');
+   }
 ?>
