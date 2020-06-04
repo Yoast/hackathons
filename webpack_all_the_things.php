81df08868fbb224164c8034b8e6925878249e635
Dieter <dieter@yoast.com>
2020-06-04 10:02:30 +0200

    initial commit

---
+++ webpack_all_the_things.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: Webpack all the things
+   Plugin URI: http://Webpackallthethings.com
+   description: A plugin that Webpacks all the things
+   Version: 1.0
+   Author: Agnieszka & Dieter   
+   Author URI: http://webpacktest.com
+   License: GPL2
+   */
+?>
