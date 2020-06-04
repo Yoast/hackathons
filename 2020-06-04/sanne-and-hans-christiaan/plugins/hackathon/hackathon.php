7d71895f22ba91c191fff39463be04bce4ed30fe
Hans-Christiaan <hans-christiaan@yoast.com>
2020-06-04 10:17:30 +0200

    Added basic plugin file.

---
+++ 2020-06-04/sanne-and-hans-christiaan/plugins/hackathon/hackathon.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Hackathon Sanne en Hans-Christiaan
+Plugin URI: https://yoast.com/
+Description: Awesome plugin!!!1!
+Version: 0.1.0
+Author: Sanne en Hans-Christiaan
+Author URI: https://yoast.com
+License: MIT
+Text Domain: hackathon-sanne-hans-christiaan
+*/
