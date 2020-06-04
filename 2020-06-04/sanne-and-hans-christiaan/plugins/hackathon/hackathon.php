34f64133a4305715ae6922fe7b1ef2548fc8a36d
Hans-Christiaan <hans-christiaan@yoast.com>
2020-06-04 11:26:37 +0200

    First working version of webpack.

--- 2020-06-04/sanne-and-hans-christiaan/plugins/hackathon/hackathon.php
+++ 2020-06-04/sanne-and-hans-christiaan/plugins/hackathon/hackathon.php
@@ -9,3 +9,20 @@ Author URI: https://yoast.com
 License: MIT
 Text Domain: hackathon-sanne-hans-christiaan
 */
+
+function show_admin_page() {
+	wp_enqueue_script( 'hackathon', plugins_url() . '/hackathon/assets/build/js/hackathon.js' );
+	echo '<h1>Hello world!</h1>';
+}
+
+function register_admin_page() {
+	add_menu_page(
+		'Admin',
+		'admin',
+		'read',
+		'admin',
+		'show_admin_page'
+	);
+}
+
+add_action( 'admin_menu', 'register_admin_page' );
