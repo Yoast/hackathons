b11ea87da2b578ffab8279a9cb8fea853f19e244
Maarten Leenders <maarten@yoast.com>
2020-06-04 09:52:42 +0200

    Add plugin text

---
+++ 2020-06-04/aida_maarten_plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Aida Maarten Plugin
+* Plugin URI: https://yoast.com/
+* Description: This is the very first plugin we ever created. Woopwoop!
+* Version: 1.0
+* Author: Aida and Maarten
+* Author URI: http://yoast.com/
+**/
